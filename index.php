7ee7b75b5f6391d844bf317c1261a77e3145c1dd
talhaamir454 <101354766+talhaamir454@users.noreply.github.com>
2023-01-13 11:04:51 +0500

    Add files via upload

--- index.php
+++ index.php
@@ -11,32 +11,30 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <h1></h1>
-    <?php
+    <h1>ajax</h1>
+    <button id="btn" onclick="" >clcik</button>
+    <div id="text"></div>
 
-     $xmldata = simplexml_load_file("https://www.geo.tv/rss/1/1");
-     $i = 1;
-    foreach ($xmldata->channel->item as $data){
-       
-    ?>
-<div class="accordion" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header" id="headingOne">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse<?php echo $i ?>" aria-expanded="true" aria-controls="collapseOne">
-<?php echo $data-> title ?>
-      </button>
-    </h2>
-    <div id="collapse<?php echo $i ?>" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-       <?php echo $data-> description ?>
-      </div>
-    </div>
-  </div>
-  <?php $i++; } ?>
-</div>
+    <script>
+     function get(){|
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("text").innerHTML = this.responseText;
+      }
+    };
+    xmlhttp.open("GET","show.php",true);
+    xmlhttp.send();
+  }
+    
+    
+    
+
+    </script>
+
    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
- 
+   
   </body>
 </html>
