86cedc6b65e5d30fe386977c9f833cafc65a47e1
hafsheikh <112744206+hafsheikh@users.noreply.github.com>
2023-01-20 12:00:57 +0500

    Add files via upload

--- edit.php
+++ edit.php
@@ -1,48 +1,51 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Edit</title>
-  </head>
-  <body>
-  
-
 <?php
-
-include("connection.php");
-
-$data = mysqli_query($con,"select * from userss where id = ".$_GET["id"]." ");
-if($std_data = mysqli_fetch_array($data)){
-?>
-
-<div class="container">
-    <input type="checkbox" id="check">
-    <div class="login form">
-      <header>EDIT FORM</header>
-      <form method='POST'>
-        <input type="text" name="first" placeholder="Enter your First Name" required>
-        <input type="text" name="last" placeholder="Enter your Last Name" required>
-        <input type="text" name="email" placeholder="Enter your email" required>
-        <input name="btn" type="submit" class="button" value="Submit" required>
-      </form>
-
-<?php } ?>
-
-
-
-<?php 
-
-include("connection.php");
-if(isset($_POST["btn"])){
-
-    mysqli_query($con,"update userss set first_name = '".$_POST["first"]."' ,last_name = '".$_POST["last"]."',email = '".$_POST["email"]."'  where id = ".$_GET["id"]." ");
-    echo "<script>alert('Updated')</script>";
-    echo "<script>window.location.assign('show_data.php')</script>";
-    
+require_once("db.php");
+if(!empty($_POST["save_record"])) {
+	$pdo_statement=$pdo_conn->prepare("update posts set post_title='" . $_POST[ 'post_title' ] . "', description='" . $_POST[ 'description' ]. "', post_at='" . $_POST[ 'post_at' ]. "' where id=" . $_GET["id"]);
+	$result = $pdo_statement->execute();
+	if($result) {
+		header('location:index.php');
+	}
 }
-
+$pdo_statement = $pdo_conn->prepare("SELECT * FROM posts where id=" . $_GET["id"]);
+$pdo_statement->execute();
+$result = $pdo_statement->fetchAll();
 ?>
+<html>
+<head>
+<title>PHP PDO CRUD - Edit Record</title>
+<style>
+body{width:615px;font-family:arial;letter-spacing:1px;line-height:20px;}
+.button_link {color:#FFF;text-decoration:none; background-color:#428a8e;padding:10px;}
+.frm-add {border: #c3bebe 1px solid;
+    padding: 30px;}
+.demo-form-heading {margin-top:0px;font-weight: 500;}	
+.demo-form-row{margin-top:20px;}
+.demo-form-field{width:300px;padding:10px;}
+.demo-form-submit{color:#FFF;background-color:#414444;padding:10px 50px;border:0px;cursor:pointer;}
+</style>
+</head>
+<body>
+<div style="margin:20px 0px;text-align:right;"><a href="index.php" class="button_link">Back to List</a></div>
+<div class="frm-add">
+<h1 class="demo-form-heading">Edit Record</h1>
+<form name="frmAdd" action="" method="POST">
+  <div class="demo-form-row">
+	  <label>Title: </label><br>
+	  <input type="text" name="post_title" class="demo-form-field" value="<?php echo $result[0]['post_title']; ?>" required  />
+  </div>
+  <div class="demo-form-row">
+	  <label>Description: </label><br>
+	  <textarea name="description" class="demo-form-field" rows="5" required ><?php echo $result[0]['description']; ?></textarea>
+  </div>
+  <div class="demo-form-row">
+	  <label>Date: </label><br>
+	  <input type="date" name="post_at" class="demo-form-field" value="<?php echo $result[0]['post_at']; ?>" required />
+  </div>
+  <div class="demo-form-row">
+	  <input name="save_record" type="submit" value="Save" class="demo-form-submit">
+  </div>
+  </form>
+</div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,40 +1,49 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
-  </head>
-  <body>
-    <h1>ajax</h1>
-    <button id="btn" onclick="" >clcik</button>
-    <div id="text"></div>
-
-    <script>
-     function get(){|
-      var xmlhttp = new XMLHttpRequest();
-      xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("text").innerHTML = this.responseText;
-      }
-    };
-    xmlhttp.open("GET","show.php",true);
-    xmlhttp.send();
-  }
-    
-    
-    
-
-    </script>
-
-   
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-   
-  </body>
+<?php
+require_once("db.php");
+?>
+<html>
+<head>
+<title>PHP PDO CRUD</title>
+<style>
+body{width:615px;font-family:arial;letter-spacing:1px;line-height:20px;}
+.tbl-qa{width: 100%;font-size:0.9em;background-color: #f5f5f5;}
+.tbl-qa th.table-header {padding: 5px;text-align: left;padding:10px;}
+.tbl-qa .table-row td {padding:10px;background-color: #FDFDFD;vertical-align:top;}
+.button_link {color:#FFF;text-decoration:none; background-color:#428a8e;padding:10px;}
+</style>
+</head>
+<body>
+<?php	
+	$pdo_statement = $pdo_conn->prepare("SELECT * FROM posts ORDER BY id DESC");
+	$pdo_statement->execute();
+	$result = $pdo_statement->fetchAll();
+?>
+<div style="text-align:right;margin:20px 0px;"><a href="add.php" class="button_link"><img src="crud-icon/add.png" title="Add New Record" style="vertical-align:bottom;" /> Create</a></div>
+<table class="tbl-qa">
+  <thead>
+	<tr>
+	  <th class="table-header" width="20%">Title</th>
+	  <th class="table-header" width="40%">Description</th>
+	  <th class="table-header" width="20%">Date</th>
+	  <th class="table-header" width="5%">Actions</th>
+	</tr>
+  </thead>
+  <tbody id="table-body">
+	<?php
+	if(!empty($result)) { 
+		foreach($result as $row) {
+	?>
+	  <tr class="table-row">
+		<td><?php echo $row["post_title"]; ?></td>
+		<td><?php echo $row["description"]; ?></td>
+		<td><?php echo $row["post_at"]; ?></td>
+		<td><a class="ajax-action-links" href='edit.php?id=<?php echo $row['id']; ?>'><img src="crud-icon/edit.png" title="Edit" /></a> <a class="ajax-action-links" href='delete.php?id=<?php echo $row['id']; ?>'><img src="crud-icon/delete.png" title="Delete" /></a></td>
+	  </tr>
+    <?php
+		}
+	}
+	?>
+  </tbody>
+</table>
+</body>
 </html>
